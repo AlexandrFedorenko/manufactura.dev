d97ace2e3e8ac7498b5dda0dc35b223afc1c730d
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-03-19 18:02:00 +0200

    add 2 sliders

--- app/js/sliders.js
+++ app/js/sliders.js
@@ -9,5 +9,88 @@ $(document).ready(function(){
         dots:true
     });
 
+    //best slider
+    var best = $('.best-item');
+    best.owlCarousel({
+        loop:true,
+        items: 3,
+        autoplay:true,
+        margin:60,
+        dots:true,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+                center: true,
+                nav:false
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+// Go to the next item
+    $('.best-item-top-right-next').click(function() {
+        best.trigger('next.owl.carousel');
+    });
+// Go to the previous item
+    $('.best-item-top-right-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        best.trigger('prev.owl.carousel', [300]);
+    });
+
+
+    //nev slider
+    var nev = $('.nev-item');
+    nev.owlCarousel({
+        loop:true,
+        items: 3,
+        autoplay:true,
+        margin:60,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+                center: true,
+                nav:false
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    // Go to the next item
+    $('.nev-item-top-right-next').click(function() {
+        nev.trigger('next.owl.carousel');
+    });
+    // Go to the previous item
+    $('.nev-item-top-right-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        nev.trigger('prev.owl.carousel', [300]);
+    });
+
+
+
 
 });
