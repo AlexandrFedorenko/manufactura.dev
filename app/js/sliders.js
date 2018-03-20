a96ed4d13a186b44f47c010b3ada63c7ce113a6e
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-03-20 17:36:51 +0200

    add index

--- app/js/sliders.js
+++ app/js/sliders.js
@@ -90,6 +90,175 @@ $(document).ready(function(){
         nev.trigger('prev.owl.carousel', [300]);
     });
 
+    //nev slider
+    var magNev = $('.mag-nev-item');
+    magNev.owlCarousel({
+        loop:true,
+        items: 3,
+        autoplay:true,
+        margin:60,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+                center: true,
+                nav:false
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    // Go to the next item
+    $('.nev-mag-top-right-next').click(function() {
+        magNev.trigger('next.owl.carousel');
+    });
+    // Go to the previous item
+    $('.nev-mag-top-right-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        magNev.trigger('prev.owl.carousel', [300]);
+    });
+
+
+
+    //nev slider
+    var topp = $('.top-item');
+    topp.owlCarousel({
+        loop:true,
+        items: 3,
+        autoplay:true,
+        margin:60,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+                center: true,
+                nav:false
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    // Go to the next item
+    $('.top-item-top-right-next').click(function() {
+        topp.trigger('next.owl.carousel');
+    });
+    // Go to the previous item
+    $('.top-item-top-right-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        topp.trigger('prev.owl.carousel', [300]);
+    });
+
+
+
+    //best-po slider
+    var bes = $('.best-pod-item');
+    bes.owlCarousel({
+        loop:true,
+        items: 3,
+        autoplay:true,
+        margin:60,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+                center: true,
+                nav:false
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    // Go to the next item
+    $('.best-pod-item-top-right-next').click(function() {
+        bes.trigger('next.owl.carousel');
+    });
+    // Go to the previous item
+    $('.best-pod-item-top-right-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        bes.trigger('prev.owl.carousel', [300]);
+    });
+
+
+
+
+
+    //best-po slider
+    var news = $('.news-item');
+    news.owlCarousel({
+        loop:true,
+        items: 2,
+        autoplay:true,
+        margin:15,
+        dots:false,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+                center: true,
+                nav:false
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    // Go to the next item
+    $('.news-item-top-right-next').click(function() {
+        news.trigger('next.owl.carousel');
+    });
+    // Go to the previous item
+    $('.news-item-top-right-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        news.trigger('prev.owl.carousel', [300]);
+    });
+
+
 
 
 
